50aea462d4d85500c2005ee6ad2cbe545469f590
Adam Alred <adam.alred34@gmail.com>
2019-12-06 14:35:30 +0000

    Add a multiplication function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
-
+def multiTwoNumbers(num1, num2):
+  num3 = num1 * num2
+  return num3
 
 def addTwoNumber(num1, num2):
   num3 = num1 + num2
@@ -8,4 +10,6 @@ def addTwoNumber(num1, num2):
 
 if __name__ == '__main__':
   answer = addTwoNumber(5, 3)
+  answer2 = multiTwoNumbers(4, 5)
   print(answer)
+  print(answer2)
